7c237acfc15e81fa4a62472cb06a2c1413d88822
MilanaBurkova <144315149+MilanaBurkova@users.noreply.github.com>
2023-10-08 20:59:25 +0300

    Add files via upload

---
+++ Лабораторная работа 1/task_3.py
@@ -0,0 +1,12 @@
+list_players = ["Маша", "Петя", "Саша", "Оля", "Кирилл", "Коля"]
+
+# TODO Разделите участников на две команды
+
+number_of_players = len(list_players)
+middle_index = number_of_players // 2
+
+first_team = list_players[:middle_index]
+second_team = list_players[middle_index:]
+
+print(first_team)
+print(second_team)
