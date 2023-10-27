ff5dbf1cc24a0c49cd86c4f47ca87228c8cf40c9
MilanaBurkova <144315149+MilanaBurkova@users.noreply.github.com>
2023-10-27 11:01:24 +0300

    Add files via upload

---
+++ Лабораторная работа 3/task_2.py
@@ -0,0 +1,14 @@
+# TODO Напишите функцию find_common_participants
+def find_common_participants(first_group, second_group, delimiter=","):
+    intersection_group = set(first_group.split(delimiter)).intersection(list(second_group.split(delimiter)))
+    result = list(intersection_group)
+    result.sort()
+    return result
+
+
+participants_first_group = "Иванов|Петров|Сидоров"
+participants_second_group = "Петров|Сидоров|Смирнов"
+
+# TODO Провеьте работу функции с разделителем отличным от запятой
+general_participants = find_common_participants(participants_first_group, participants_second_group)
+print(general_participants)
